c49d01fc53ce69539675a9cd98d8bbf4919716cd
User <User@DESKTOP-7R6GJ3H>
2021-12-26 19:05:57 +0200

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using job11;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Math.Round(Math.Log10(2-1),4),
+                Math.Round(job11.Program.Ln(Math.Log10(2-1))));
+        }
+    }
+}
---
+++ job1.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace job11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(Math.Log10(5 - 1)));
+            Console.ReadKey();
+        }
+        public static double Ln(double x)
+        {
+            const double eps = 1e-15;  // погрешность вычислений
+            double s = 0;  // начальная сумма
+            double r = x;   // первый член ряда
+            int n = 1;         // показатель степени
+            while (Math.Abs(r) > eps) // условие выполнения цикла
+            {
+                s = s + r;      // добавление члена ряда
+                n = n + 2;  // наращивание  n:  1,3,5,7,...
+                r = -r * Math.Log10(x - 1); // новый член ряда
+            }
+            return s;       // возврат результата
+        }
+    }
+    
+}
